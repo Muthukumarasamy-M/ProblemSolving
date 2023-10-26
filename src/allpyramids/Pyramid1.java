b1140b655986f354c79000ff7bc8cca84e495971
ramki <ramki@172.17.49.61>
2023-10-26 13:00:59 +0530

    initial commit

--- src/allpyramids/Pyramid1.java
+++ src/allpyramids/Pyramid1.java
@@ -5,18 +5,17 @@ import java.util.Scanner;
 public class Pyramid1 {
 
 	public static void main(String[] args) {
-		
-		Scanner mc= new Scanner(System.in);
+
+		Scanner mc = new Scanner(System.in);
 		System.out.println("enter the number");
-		int n=mc.nextInt();
-		
-		for(int i=1;i<=n;i++)
-		{
-			for(int k=n-i;k>=0;k--)
+		int n = mc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			for (int k = n - i; k >= 0; k--)
 				System.out.print(" ");
-			for(int j=1;j<=i;j++)
-				
-				System.out.print(i+" ");
+			for (int j = 1; j <= i; j++)
+
+				System.out.print(i + " ");
 			System.out.println();
 		}
 	}
--- src/allpyramids/Pyramid2.java
+++ src/allpyramids/Pyramid2.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Pyramid2 {
 
 	public static void main(String[] args) {
-		
-		Scanner mc= new Scanner(System.in);
+
+		Scanner mc = new Scanner(System.in);
 		System.out.println("enter the number");
-		int n=mc.nextInt();
-		
-		for(int i=n;i>=1;i--)
-		{
-			for(int k=n-i;k>=1;k--)
+		int n = mc.nextInt();
+
+		for (int i = n; i >= 1; i--) {
+			for (int k = n - i; k >= 1; k--)
 				System.out.print(" ");
-			for(int j=1;j<=i;j++)
-				System.out.print(i+" ");
+			for (int j = 1; j <= i; j++)
+				System.out.print(i + " ");
 			System.out.println();
-		}}
+		}
+	}
 }
 /*
 
