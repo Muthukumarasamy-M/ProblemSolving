21afd51c34f58330afbdb78c0564b75e92712508
ramki <ramki@172.17.49.61>
2023-11-07 17:37:28 +0530

    initial commit

---
+++ src/com/muthukumarasamym/leetcode/MoveZeros.java
@@ -0,0 +1,64 @@
+package com.muthukumarasamym.leetcode;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MoveZeros {
+
+	public static void main(String[] args) {
+		
+		Scanner mc=new Scanner(System.in);
+		System.out.println("enter the number");
+		int n= mc.nextInt();
+		int a[]=new int[n];
+		System.out.println("enter the numbers");
+		for(int i=0;i<n;i++)
+		{
+			a[i]=mc.nextInt();
+		}
+		moveZeroes(a);
+		moveZeroes2(a);
+	}
+	private static void moveZeroes2(int[] a) {
+		int i = 0; 
+        for (int num:a){
+            if(num != 0){
+                a[i] = num;
+                i++;
+            }
+        }
+        while(i<a.length){
+            a[i] = 0;
+            i++;
+        }
+        System.out.println(Arrays.toString(a)+" 2");
+	}
+	
+	
+	
+	public static void moveZeroes(int[] a) {
+        int k=0,i;
+		for( i=0;i<a.length;i++)
+		{
+			
+			if(a[i]==0)
+			{
+				int j=i+1;
+				while(j<a.length)
+				{
+					if(a[j]!=0)
+					{
+						int temp= a[i];
+						a[i]=a[j];
+						a[j]=temp;
+						break;
+					}
+					j++;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(a)+" "+k);
+        
+    }
+
+}
