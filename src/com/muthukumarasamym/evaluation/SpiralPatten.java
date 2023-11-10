d4cd76d8a493b895af77ef6296f22d2399fa38ff
ramki <ramki@172.17.49.61>
2023-11-10 17:54:47 +0530

    initial commit

---
+++ src/com/muthukumarasamym/evaluation/SpiralPatten.java
@@ -0,0 +1,15 @@
+package com.muthukumarasamym.evaluation;
+
+import java.util.Scanner;
+
+public class SpiralPatten {
+
+	public static void main(String[] args) {
+		Scanner mc= new Scanner(System.in);
+		System.out.println("enter the number");
+        int n = mc.nextInt();
+        
+
+	}
+
+}
