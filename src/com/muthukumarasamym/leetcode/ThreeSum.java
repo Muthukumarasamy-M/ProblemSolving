0072d1a10380ce97752ddd55def3f4e1858ba936
ramki <ramki@172.17.49.61>
2023-11-15 18:20:10 +0530

    initial commit

---
+++ src/com/muthukumarasamym/leetcode/ThreeSum.java
@@ -0,0 +1,22 @@
+package com.muthukumarasamym.leetcode;
+
+import java.util.Scanner;
+
+public class ThreeSum {
+
+	public static void main(String[] args) {
+		Scanner mc= new Scanner(System.in);
+		System.out.println("Enter the length of Array");
+		int n=mc.nextInt();
+		int arr[]=new int[n];
+		System.out.println("Enter the values");
+		for(int i=0;i<n;i++)
+		{
+			
+			
+		}
+		
+
+	}
+
+}
