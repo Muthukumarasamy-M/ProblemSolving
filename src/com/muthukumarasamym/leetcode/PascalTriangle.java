46d5faadcd3309e69f8bf0e2b0962aa13af43681
ramki <ramki@172.17.49.61>
2023-11-28 14:32:11 +0530

    initial commit

--- src/com/muthukumarasamym/leetcode/PascalTriangle.java
+++ src/com/muthukumarasamym/leetcode/PascalTriangle.java
@@ -13,7 +13,7 @@ public class PascalTriangle {
 
 		PascalTriangle p = new PascalTriangle();
 		System.out.println(p.pascalTriangle(n));
-
+		System.out.println(p.pascalTriangle2(n));
 	}
 
 	private List<List<Integer>> pascalTriangle(int n) {
@@ -24,9 +24,7 @@ public class PascalTriangle {
 			
 			List<Integer> li = new ArrayList<>();
 			for (int j = 0; j <= i; j++) {
-				if (list.isEmpty()) {
-					li.add(1);
-				} else if (i == 1)
+				if (i==0||i==1) 
 					li.add(1);
 				else if (j == 0 || j == i)
 					li.add(1);
@@ -36,6 +34,26 @@ public class PascalTriangle {
 			}
 			lit = li;
 			list.add(li);
+
+		}
+		return list;
+	}
+	private List<Integer> pascalTriangle2(int n) {
+
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			
+			List<Integer> li = new ArrayList<>();
+			for (int j = 0; j <= i; j++) {
+				if (i==0||i==1) 
+					li.add(1);
+				else if (j == 0 || j == i)
+					li.add(1);
+				else
+					li.add(list.get(j - 1) + list.get(j));
+
+			}
+			list = li;
 
 		}
 		return list;
