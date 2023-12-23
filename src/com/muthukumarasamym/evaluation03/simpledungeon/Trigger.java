75ddd807fdec1f3abbd77b4f8e6c0e814b5dd388
ramki <ramki@172.17.49.61>
2023-12-23 12:27:22 +0530

    initial commit

---
+++ src/com/muthukumarasamym/evaluation03/simpledungeon/Trigger.java
@@ -0,0 +1,35 @@
+package com.muthukumarasamym.evaluation03.simpledungeon;
+
+import java.util.Scanner;
+
+public class Trigger {
+
+	public static void main(String[] args) {
+
+		Scanner mc = new Scanner(System.in);
+		System.out.println("Dimension of Dungeon (row X col) : ");
+		int row = mc.nextInt(), col = mc.nextInt();
+		System.out.println("Position of adventurer : ");
+		int arow = mc.nextInt(), acol = mc.nextInt();
+		System.out.println("Postion of Monster : ");
+		int mrow = mc.nextInt(), mcol = mc.nextInt();
+		System.out.println("Position of Trigger : ");
+		int trow = mc.nextInt(), tcol = mc.nextInt();
+		System.out.println("Position of Gold : ");
+		int grow = mc.nextInt(), gcol = mc.nextInt();
+		int adventuremin = Math.abs(arow - grow) + Math.abs(acol - gcol);
+		int monstermin = Math.abs(mrow - grow) + Math.abs(mcol - gcol);
+
+		if (adventuremin > monstermin) {
+			int Adventotrigger = Math.abs(arow - trow) + Math.abs(acol - tcol);
+			int triggertogold = Math.abs(trow - grow) + Math.abs(tcol - gcol);
+			System.out.println("minimum number of steps is  : " + (Adventotrigger + triggertogold));
+
+		} else {
+			System.out.println("minimum number of steps is  : " + adventuremin);
+
+		}
+
+	}
+
+}
